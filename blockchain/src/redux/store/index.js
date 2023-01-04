6643830f8fb497521846ed6f67344393ac5377ec
Nic <nic.dan.costea@gmail.com>
2023-01-04 17:49:37 +0100

    REDUX-persist&encrypt

--- blockchain/src/redux/store/index.js
+++ blockchain/src/redux/store/index.js
@@ -1,4 +1,4 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore, combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
 // import utilsReducer from "../reducers/utilsReducer";
 import plannerReducer from "./reducers/plannerReducer";
 import taskReducer from "./reducers/plannerReducer";
@@ -6,9 +6,16 @@ import taskReducer from "./reducers/plannerReducer";
 import localStorage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
 
+import { encryptTransform } from "redux-persist-transform-encrypt";
+
 const persistConfig = {
   key: "root",
   storage: localStorage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_SECRET_KEY,
+    }),
+  ],
 };
 
 const mainReducer = combineReducers({
@@ -20,6 +27,11 @@ const persistedReducer = persistReducer(persistConfig, mainReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    });
+  },
 });
 
 export const persistor = persistStore(store);
